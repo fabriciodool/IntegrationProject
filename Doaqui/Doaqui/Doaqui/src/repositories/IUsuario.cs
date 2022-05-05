ee469b4b20ba7076542b1c8415e59725ffdb28d5
Erick Chiappone <erick_braga_11@hotmail.com>
2022-05-05 17:50:03 -0300

    @echiappone [teste]{05/05/2022 {17:49}

---
+++ Doaqui/Doaqui/Doaqui/src/repositories/IUsuario.cs
@@ -0,0 +1,12 @@
+﻿namespace Doaqui.src.repositories
+{
+    /// <summary>
+    /// <para>Resumo: Responsavel por representar ações de CRUD de usuario</para>
+    /// <para>Criado por: Grupo 2</para>
+    /// <para>Versão: 1.0</para>
+    /// <para>Data: 05/05/2022</para>
+    /// </summary>
+    public interface IUsuario
+    {
+    }
+}
